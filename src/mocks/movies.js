49a8c5444900eafcd24002d4b7f5e5453939779d
Jose Artur <jose.artur.coelho@ccc.ufcg.edu.br>
2023-08-29 12:36:05 -0300

    adicao dos dados dos filmes

---
+++ src/mocks/movies.js
@@ -0,0 +1,122 @@
+export default movies = [
+  {
+    title: "Parasite",
+    length: "152min",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
+    id: 1,
+    year: 2019,
+    genres: "Drama/Suspense",
+    price: "3",
+  },
+  {
+    title: "Blade Runner",
+    length: "118min",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg",
+    id: 2,
+    year: 1982,
+    genres: "Ficção cientifíca/Policial",
+    price: "4",
+  },
+  {
+    title: "The Grand Budapest Hotel",
+    length: "100min",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BMzM5NjUxOTEyMl5BMl5BanBnXkFtZTgwNjEyMDM0MDE@._V1_.jpg",
+    id: 3,
+    year: 2014,
+    genres: "Comédia",
+    price: "2",
+  },
+  {
+    title: "Before Sunrise",
+    length: "101min",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BZDdiZTAwYzAtMDI3Ni00OTRjLTkzN2UtMGE3MDMyZmU4NTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+    id: 4,
+    year: 1995,
+    genres: "Romance",
+    price: "5",
+  },
+  {
+    title: "Kill Bill: Vol. 1",
+    length: "111min",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BNzM3NDFhYTAtYmU5Mi00NGRmLTljYjgtMDkyODQ4MjNkMGY2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
+    id: 5,
+    year: 2003,
+    genres: "Ação",
+    price: "4",
+  },
+  {
+    title: "2001: A Space Odyssey",
+    length: "149min",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BMmNlYzRiNDctZWNhMi00MzI4LThkZTctMTUzMmZkMmFmNThmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    id: 6,
+    year: 1968,
+    genres: "Ficção Científica",
+    price: "7",
+  },
+  {
+    title: "La La Land",
+    length: "129min",
+    cover:
+      "https://m.media-amazon.com/images/I/61ddZ2RwnHL._AC_UF894,1000_QL80_.jpg",
+    id: 7,
+    year: 2016,
+    genres: "Romance/Musical",
+    price: "4",
+  },
+  {
+    title: "My Neighbor Totoro",
+    length: "86min",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BMzM5NjUxOTEyMl5BMl5BanBnXkFtZTgwNjEyMDM0MDE@._V1_.jpg",
+    id: 8,
+    year: 1988,
+    genres: "Aventura/Animação",
+    price: "5",
+  },
+  {
+    title: "GoodFellas",
+    length: "145min",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
+    id: 9,
+    year: 1990,
+    genres: "Crime/Drama",
+    price: "8",
+  },
+  {
+    title: "Paddington",
+    length: "96min",
+    cover:
+      "https://m.media-amazon.com/images/I/81ajUVs6ddL._AC_UF894,1000_QL80_.jpg",
+    id: 10,
+    year: 2014,
+    genres: "Comédia/Aventura",
+    price: "6",
+  },
+  {
+    title: "The Worst Person in the World",
+    length: "128min",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BOWZkMGY2MzgtMTA0My00OTAxLTk0MDEtNWNjMWVhZGMyNGJiXkEyXkFqcGdeQXVyMTk4NTIzMzI@._V1_.jpg",
+    id: 11,
+    year: 2021,
+    genres: "",
+    price: "4",
+  },
+  {
+    title: "The Seventh Seal",
+    length: "96min",
+    cover:
+      "https://darkroom.bbfc.co.uk/751/https://m.media-amazon.com/images/M/MV5BM2I1ZWU4YjMtYzU0My00YmMzLWFmNTAtZDJhZGYwMmI3YWQ5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg:fa551377e3646aa2f2a2039f5e71e769",
+    id: 12,
+    year: 1957,
+    genres: "Fantasia/Drama",
+    price: "6",
+  },
+];
